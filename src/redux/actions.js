9ffd860dadf407c4505921811800601a0a1fe2a3
JabedH <jabedhtusar@gmail.com>
2022-10-19 12:39:36 +0600

    added start section

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -7,9 +7,42 @@ import {
   QUIZ_TIMEOUT,
 } from "./actionsTypes";
 
-export const startQuiz = (time)=>{
-    return {
-        type: QUIZ_START,
-        payload: time
-    }
-}
+export const startQuiz = (time) => {
+  return {
+    type: QUIZ_START,
+    payload: time,
+  };
+};
+export const nextQuiz = (answers) => {
+  return {
+    type: QUIZ_NEXT,
+    payload: answers,
+  };
+};
+export const prevQuiz = () => {
+  return {
+    type: QUIZ_PREV,
+    payload: null,
+  };
+};
+export const submitQuiz = (answers, time) => {
+  return {
+    type: QUIZ_START,
+    payload: {
+      answers: answers,
+      time: time,
+    },
+  };
+};
+export const resetQuiz = () => {
+  return {
+    type: QUIZ_RESET,
+    payload: null,
+  };
+};
+export const timeOut = () => {
+  return {
+    type: QUIZ_TIMEOUT,
+    payload: null,
+  };
+};
